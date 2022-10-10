5467684e747ee1d4fc31836322f5eef9df0e3c43
p-a <peter.augustinak@gmail.com>
2022-10-10 11:09:33 +0200

    Updated ALLOWED_HOSTS fo actual heroku app link

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,7 +31,12 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = [
+    "blooming-everglades-97468.herokuapp.com ",
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0"
+]
 
 # Application definition
 
